c7bc4475bfdd26ffe025b496a2dcd9fdcd8ae036
Jan cool <jan.joris.cool@gmail.com>
2020-06-07 19:12:55 +0200

    version 4.5
    player jump and removing render function

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def handle_events(events, quit_rect, test_rect):
     handle_events_dict = {}
     no_movement = False
     no_ctrl = False
+    no_space = False
     
     for event in events:
         if event.type == pygame.QUIT:
@@ -39,7 +40,10 @@ def handle_events(events, quit_rect, test_rect):
             
             elif event.key == 1073742048:
                 handle_events_dict['ctrl_down'] = True
-                no_ctrl = True
+                no_ctrl = True     
+            elif event.key == 32:
+                handle_events_dict['space_down'] = True
+                no_space = True
     
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_w:
@@ -57,134 +61,18 @@ def handle_events(events, quit_rect, test_rect):
             elif event.key == 1073742048:
                 handle_events_dict['ctrl_down'] = False
                 no_ctrl = True
+            elif event.key == 32:
+                handle_events_dict['space_down'] = False
+                no_space = True
         
     if no_movement != True:
         handle_events_dict['movement_event_happened'] = False
-        
     if no_ctrl != True:
         handle_events_dict['ctrl_event_happened'] = False
+    if no_space != True:
+        handle_events_dict['space_event_happened'] = False
     
     return handle_events_dict
-
-def rects_render(background_list, gameDisplay, w_down, s_down, a_down, d_down, player_x, player_y, player_speed, deltatime, unsolid_moverect_list, solid_moverect_list, player_rect, UI_rects, gravity, ctrl_down, schale, player_sprite, width, height):
-    player_x = 0
-    player_y = 0
-    player_movement_allow = True
-    gravity_movement_allow = True
-    one_pixel_movement_allow = True
-    in_block = False
-
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    GRAY = (30, 30, 30)
-    GREEN = (0, 255, 0)
-    BLUE = (0, 0, 255)
-
-    if w_down:
-        player_y -= 1 * player_speed
-    elif s_down:
-        player_y += 1 * player_speed
-    elif a_down:
-        player_x -= 1 * player_speed
-    elif d_down:
-        player_x += 1 * player_speed
-        
-    if ctrl_down:
-        player_rect.height -= 32
-        player_rect.top += 32
-    
-    #look if you can add gravity
-    for y in solid_moverect_list:
-        y.top -= gravity
-        if player_rect.colliderect(y) == True:
-            gravity_movement_allow = False
-        y.top += gravity
-
-    #add gravity
-    if gravity_movement_allow == True:
-        for y in solid_moverect_list:
-            y.top -= gravity
-        for x in unsolid_moverect_list:
-            x.top -= gravity
-    
-    #look if you can add button movement
-    for rect in solid_moverect_list:
-        rect.top -= player_y
-        rect.left -= player_x
-        if player_rect.colliderect(rect) == True:
-            player_movement_allow = False
-        rect.top += player_y
-        rect.left += player_x
-
-    #add button movement
-    if player_x != 0 or player_y != 0:
-        if player_movement_allow == True:
-            for rect in solid_moverect_list:
-                rect.top -= player_y
-                rect.left -= player_x
-            for rect in unsolid_moverect_list:
-                rect.top -= player_y
-                rect.left -= player_x
-    
-    #look if you can add 1 pixel down
-    for y in solid_moverect_list:
-        y.top -= 1
-        if player_rect.colliderect(y) == True:
-            one_pixel_movement_allow = False
-        y.top += 1
-
-    #add 1 pixel down
-    if gravity_movement_allow != True:
-        if one_pixel_movement_allow == True:
-            for y in solid_moverect_list:
-                y.top -= 1
-            for x in unsolid_moverect_list:
-                x.top -= 1
-    
-    #look if your in a block
-    for rect in solid_moverect_list:
-        if player_rect.colliderect(rect) == True:
-            in_block = True
-    
-    #push you up if your stuck in a block
-    if in_block == True:
-        print("stuck")
-        for rect in solid_moverect_list:
-            rect.top += gravity
-        for rect in unsolid_moverect_list:
-            rect.top += gravity
-    
-    #background
-    gameDisplay.blit(background_list[0], (0, 0))
-
-    #floor
-    pygame.draw.rect(gameDisplay, (16, 89, 15), solid_moverect_list[0])
-    pygame.draw.rect(gameDisplay, (150, 85, 6), solid_moverect_list[2])
-
-    #for ground
-    pygame.draw.rect(gameDisplay, GREEN, unsolid_moverect_list[1])
-    pygame.draw.rect(gameDisplay, (122, 84, 38), unsolid_moverect_list[2])
-    pygame.draw.rect(gameDisplay, GREEN, unsolid_moverect_list[3])
-    pygame.draw.rect(gameDisplay, BLUE, unsolid_moverect_list[4])
-    pygame.draw.rect(gameDisplay, BLUE, unsolid_moverect_list[5])
-    pygame.draw.rect(gameDisplay, BLUE, unsolid_moverect_list[6])
-    pygame.draw.rect(gameDisplay, (122, 84, 38), unsolid_moverect_list[0])
-    pygame.draw.rect(gameDisplay, (107, 58, 22), solid_moverect_list[1])
-    
-    #debugging level
-    pygame.draw.rect(gameDisplay, GREEN, player_rect)      #enable for player collider
-    
-    #player
-    gameDisplay.blit(player_sprite, (int(width/2-32), int(height/2-32)))
-
-    #ui level
-    pygame.draw.rect(gameDisplay, (33, 33, 33), UI_rects[1])
-    pygame.draw.rect(gameDisplay, (23, 23, 23), UI_rects[0])
-
-    if ctrl_down:
-        player_rect.height += 32
-        player_rect.top -= 32
-
 
 def main():
     width = 1000 #16*50
@@ -192,13 +80,10 @@ def main():
     fps = 60
     schale = 2
     character_bigness = 2
-    world_seed = random.randint(1, 9999999999)
-
-    player_x = 0
-    player_y = 0
     player_speed = 5
-
     gravity = 5
+    jump_lenght = 20 #how many frames you jump
+    jump_speed = 30 #pixel you jump per second
 
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
@@ -241,25 +126,44 @@ def main():
     UI_rects = [quit_rect, test_rect]
     background_list = [wallpaper]
 
-    w_down, s_down, a_down, d_down, ctrl_down = False, False, False, False, False
+    w_down, s_down, a_down, d_down, ctrl_down, space_down = False, False, False, False, False, False
     deltatime = 0 #don't chanche this
     mainloop = True
+    player_on_the_ground = False
+    world_seed = random.randint(1, 9999999999)
+    player_x = 0
+    player_y = 0
+    first_frame = True
+    temp_jump_lenght = 0
+    can_jump = True
     
     while mainloop:
         events = pygame.event.get()
         mouse = pygame.mouse.get_pos()
         action = handle_events(events, quit_rect, test_rect)
+        player_x = 0
+        player_y = 0
+        temp_jump = 0
+        player_movement_allow = True
+        gravity_movement_allow = True
+        jump_movement_allow = True
+        one_pixel_movement_allow = True
+        in_block = False
+        player_on_the_ground = False
+        jumping = False
 
         exit = action.get('exit')
         movement_event_happened = action.get('movement_event_happened')
         ctrl_event_happened = action.get('ctrl_event_happened')
+        space_event_happened = action.get('space_event_happened')
         testPressed = action.get('test')
         
         if movement_event_happened != False:
             w_down, s_down, a_down, d_down = action.get('w_down'), action.get('s_down'), action.get('a_down'), action.get('d_down')
-            
         if ctrl_event_happened != False:
             ctrl_down = action.get('ctrl_down')
+        if space_event_happened != False:
+            space_down = action.get('space_down')
 
         if exit:
             return True
@@ -267,13 +171,150 @@ def main():
         if testPressed:
             print("test button")
 
-        gameDisplay.fill(GRAY)
-
-        rects_render(background_list, gameDisplay, w_down, s_down, a_down, d_down, player_x, player_y, player_speed, deltatime, unsolid_moverect_list, solid_moverect_list, player_rect, UI_rects, gravity, ctrl_down, schale, player_sprite, width, height)
+        if w_down:
+            player_y -= 0 * player_speed # w is disabeld
+        elif s_down:
+            player_y += 0 * player_speed # s is disabeld
+        elif a_down:
+            player_x -= 1 * player_speed
+        elif d_down:
+            player_x += 1 * player_speed
+            
+        if ctrl_down:
+            player_rect.height -= 32
+            player_rect.top += 32
         
+        #kijk of de speler op de grond is
+        for rects in solid_moverect_list:
+            rects.top -= 1
+            if player_rect.colliderect(rects) == True:
+                player_on_the_ground = True
+            rects.top += 1
+        
+        #calculate jump
+        if player_on_the_ground == True and temp_jump_lenght != 0:
+            temp_jump_lenght = 0
+            can_jump = True
+        if space_down:
+            if temp_jump_lenght == jump_lenght:
+                can_jump = False
+            if can_jump == True:
+                temp_jump_lenght += 1
+                jumping = True
+        if jumping == True:
+            player_y -= 5
+        
+        if jumping != True:
+            #look if you can add gravity
+            for rects in solid_moverect_list:
+                rects.top -= gravity
+                if player_rect.colliderect(rects) == True:
+                    gravity_movement_allow = False
+                rects.top += gravity
+
+            #add gravity
+            if gravity_movement_allow == True:
+                for rects in solid_moverect_list:
+                    rects.top -= gravity
+                for rects in unsolid_moverect_list:
+                    rects.top -= gravity
+        
+        #look if you can add button movement
+        for rect in solid_moverect_list:
+            rect.top -= player_y
+            rect.left -= player_x
+            if player_rect.colliderect(rect) == True:
+                player_movement_allow = False
+            rect.top += player_y
+            rect.left += player_x
+
+        #add button movement
+        if player_x != 0 or player_y != 0:
+            if player_movement_allow == True:
+                for rect in solid_moverect_list:
+                    rect.top -= player_y
+                    rect.left -= player_x
+                for rect in unsolid_moverect_list:
+                    rect.top -= player_y
+                    rect.left -= player_x
+        
+        #look if you can jump
+        for rects in solid_moverect_list:
+            rects.top += temp_jump
+            if player_rect.colliderect(rects) == True:
+                jump_movement_allow = False
+            rects.top -= temp_jump
+
+        #add jump
+        if jump_movement_allow == True:
+            for rects in solid_moverect_list:
+                rects.top += temp_jump
+            for rects in unsolid_moverect_list:
+                rects.top += temp_jump
+        
+        #look if you can add 1 pixel down
+        for rects in solid_moverect_list:
+            rects.top -= 1
+            if player_rect.colliderect(rects) == True:
+                one_pixel_movement_allow = False
+            rects.top += 1
+
+        #add 1 pixel down
+        if gravity_movement_allow != True:
+            if one_pixel_movement_allow == True:
+                for rects in solid_moverect_list:
+                    rects.top -= 1
+                for rects in unsolid_moverect_list:
+                    rects.top -= 1
+        
+        #look if your in a block
+        for rect in solid_moverect_list:
+            if player_rect.colliderect(rect) == True:
+                in_block = True
+        
+        #push you up if your stuck in a block
+        if in_block == True:
+            print("stuck")
+            for rect in solid_moverect_list:
+                rect.top += gravity
+            for rect in unsolid_moverect_list:
+                rect.top += gravity
+        
+        #background
+        gameDisplay.fill(GRAY)
+        gameDisplay.blit(background_list[0], (0, 0))
+
+        #floor
+        pygame.draw.rect(gameDisplay, (16, 89, 15), solid_moverect_list[0])
+        pygame.draw.rect(gameDisplay, (150, 85, 6), solid_moverect_list[2])
+
+        #for ground
+        pygame.draw.rect(gameDisplay, GREEN, unsolid_moverect_list[1])
+        pygame.draw.rect(gameDisplay, (122, 84, 38), unsolid_moverect_list[2])
+        pygame.draw.rect(gameDisplay, GREEN, unsolid_moverect_list[3])
+        pygame.draw.rect(gameDisplay, BLUE, unsolid_moverect_list[4])
+        pygame.draw.rect(gameDisplay, BLUE, unsolid_moverect_list[5])
+        pygame.draw.rect(gameDisplay, BLUE, unsolid_moverect_list[6])
+        pygame.draw.rect(gameDisplay, (122, 84, 38), unsolid_moverect_list[0])
+        pygame.draw.rect(gameDisplay, (107, 58, 22), solid_moverect_list[1])
+        
+        #debugging level
+        pygame.draw.rect(gameDisplay, GREEN, player_rect)      #enable for player collider
+        
+        #player
+        gameDisplay.blit(player_sprite, (int(width/2-32), int(height/2-32)))
+
+        #ui level
+        pygame.draw.rect(gameDisplay, (33, 33, 33), UI_rects[1])
+        pygame.draw.rect(gameDisplay, (23, 23, 23), UI_rects[0])
         gameDisplay.blit(font30.render('quit', True, WHITE), (width-60, 10))
 
+        if ctrl_down:
+            player_rect.height += 32
+            player_rect.top -= 32
+
         pygame.display.update()
+        first_frame = False
         #delta time is milliseconds since the previous call
         deltatime = clock.tick(fps)
 
