d0b75848c52864b31b8636563f906dc267617974
Jan cool <jan.joris.cool@gmail.com>
2020-06-10 15:14:31 +0200

    version 4 save 5.6
    add the scrip sprint-loader.py

---
+++ sprint-loader.py
@@ -0,0 +1,14 @@
+
+class player:
+    def __init__(self, what_animition):
+        self.what_animition = what_animition
+    
+    if what_animition == "idle":
+        idle_animation()
+    else what_animition == "run":
+        run()
+    
+    def idle_animation():
+        pass
+    def idle_animation():
+        pass
