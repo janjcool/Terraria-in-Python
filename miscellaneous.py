93c74accfaf81653c4aabf5e37cd009e0684598e
thraal <thraal@gmail.com>
2020-07-09 16:19:53 +0200

    make a template config file

--- miscellaneous.py
+++ miscellaneous.py
@@ -1,4 +1,5 @@
 import configparser
+import pprint
 
 class exit:
     def __init__(self):
@@ -12,69 +13,60 @@ class menuManager:
         config_dict["window"]["display_choser"] = str(menu_type)
 
 def config(display_info):
-    config = configparser.ConfigParser()
-    config.read('save-data/config.ini')
-    
+    files = ("config.ini", "entity_variables.ini", "template.ini")
     config_dict = {}
-    
-    for x in config.sections():
-        dictionary = {}
-        
-        for y in config[str(x)]:
-            if isint(config[str(x)][str(y)]) == True:
-                dictionary[str(y)] = int(config[str(x)][str(y)])
-                
-            elif isfloat(config[str(x)][str(y)]) == True:
-                dictionary[str(y)] = float(config[str(x)][str(y)])
-                
-            elif x == "colors":
-                color_list = []
-                
-                for i in config[str(x)][str(y)].split(", "):
-                    color_list.append(int(i))
-                dictionary[str(y)] = color_list
-                
-            else:
-                dictionary[str(y)] = config[str(x)][str(y)]
-                
-        if x == "window":
-            dictionary["display_width"] = display_info.current_w - 100
-            dictionary["display_height"] = display_info.current_h - 100
-                
-        config_dict[str(x)] = dictionary
-
-    entity_variables = configparser.ConfigParser()
-    entity_variables.read('save-data/entity_variables.ini')
-    
     variables = {}
-    
-    for x in entity_variables.sections():
-        dictionary = {}
+    templates = {}
+
+    for i in files:
+        config = configparser.ConfigParser()
+        config.read('save-data/' + str(i))
         
-        for y in entity_variables[str(x)]:
-            if isint(entity_variables[str(x)][str(y)]) == True:
-                dictionary[str(y)] = int(entity_variables[str(x)][str(y)])
+        for x in config.sections():
+            dictionary = {}
+            
+            for y in config[str(x)]:
+                if isint(config[str(x)][str(y)]) == True:
+                    dictionary[str(y)] = int(config[str(x)][str(y)])
+                    
+                elif isfloat(config[str(x)][str(y)]) == True:
+                    dictionary[str(y)] = float(config[str(x)][str(y)])
+                    
+                elif x == "colors":
+                    color_list = []
+                    
+                    for i in config[str(x)][str(y)].split(", "):
+                        color_list.append(int(i))
+                    dictionary[str(y)] = color_list
                 
-            elif isfloat(entity_variables[str(x)][str(y)]) == True:
-                dictionary[str(y)] = float(entity_variables[str(x)][str(y)])
+                elif config[str(x)][str(y)] == "False":
+                    dictionary[str(y)] = False
                 
-            elif entity_variables[str(x)][str(y)] == "False":
-                dictionary[str(y)] = False
-                
-            elif entity_variables[str(x)][str(y)] == "True":
-                dictionary[str(y)] = True
-                
-            else:
-                dictionary[str(y)] = entity_variables[str(x)][str(y)]
-        
-        if x == "general":
-            dictionary["events"] = []
-            dictionary["event_dict"] = {}
-                
-        variables[str(x)] = dictionary
-    
-    config_dict["variables"] = variables
-    
+                elif config[str(x)][str(y)] == "True":
+                    dictionary[str(y)] = True
+                    
+                else:
+                    dictionary[str(y)] = config[str(x)][str(y)]
+                    
+            print(dictionary)
+            if x == "window":
+                dictionary["display_width"] = display_info.current_w - 100
+                dictionary["display_height"] = display_info.current_h - 100
+            
+            elif x == "general":
+                dictionary["events"] = []
+                dictionary["event_dict"] = {}
+                    
+            if i == "config.ini":
+                config_dict[str(x)] = dictionary
+            elif i == "entity_variables.ini":
+                variables[str(x)] = dictionary
+            elif i == "template.ini":
+                templates[str(x)] = dictionary
+
+    config_dict["entity_variables"] = variables
+    config_dict["template"] = templates
+
     return config_dict
 
 def isfloat(x):
